53109a450463d7958ef8c9207ec35cd20a8bbdbb
Trần Tuấn Vũ <vutran165@gmail.com>
2018-10-02 10:58:18 +0700

    first commit

--- src/app/layout/contact-web/contact-web.component.ts
+++ src/app/layout/contact-web/contact-web.component.ts
@@ -4,13 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-contact-web',
   templateUrl: './contact-web.component.html',
   // styleUrls: ['./contact-web.component.css']
-  styleUrls: ['../src/styles/test_resposive.css']
+  styleUrls: ['./src/styles/test_resposive.css']
 })
 export class ContactWebComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
