b066ea8dcb720bd600a7dda34edaa5cfecbecf42
Trần Tuấn Vũ <vutran165@gmail.com>
2018-10-03 17:30:56 +0700

    4...

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const http = require('http');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/src'));
+app.use(express.static(__dirname + '/dist/company-app-bachtuvu'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8888);
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join('/src/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/company-app-bachtuvu/index.html'));
 })
 console.log('Running');
 
